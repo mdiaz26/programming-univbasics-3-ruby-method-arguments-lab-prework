a2405a1f673c1195af966d86388951e2ff4601a4
Michael Diaz <mike.diaz006@gmail.com>
2019-12-03 20:54:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -24,6 +24,13 @@ end
 
 introduction_with_language_optional("Mike")
 
+#introduction_with_language_optional
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Mike", "Python")
+
 
 
 
